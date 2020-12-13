32379a72d27c48d66b8a36ccfad4537c52d8d4c5
Jim Jagielski <jimjag@gmail.com>
2020-12-13 13:56:34 -0500

    Updates for revs

--- tests/junitTests/src/bsh/Project_Coin_Test.java
+++ tests/junitTests/src/bsh/Project_Coin_Test.java
@@ -1,3 +1,32 @@
+/*****************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one                *
+ * or more contributor license agreements.  See the NOTICE file              *
+ * distributed with this work for additional information                     *
+ * regarding copyright ownership.  The ASF licenses this file                *
+ * to you under the Apache License, Version 2.0 (the                         *
+ * "License"); you may not use this file except in compliance                *
+ * with the License.  You may obtain a copy of the License at                *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing,                *
+ * software distributed under the License is distributed on an               *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY                    *
+ * KIND, either express or implied.  See the License for the                 *
+ * specific language governing permissions and limitations                   *
+ * under the License.                                                        *
+ *                                                                           *
+ * This file is part of the BeanShell Java Scripting distribution.           *
+ * Documentation and updates may be found at                                 *
+ *      https://github.com/jimjag/beanshell2                                 *
+ *                                                                           *
+ * This file is part of the BeanShell Java Scripting distribution.           *
+ * Documentation and updates may be found at http://www.beanshell.org/       *
+ * Patrick Niemeyer (pat@pat.net)                                            *
+ * Author of Learning Java, O'Reilly & Associates                            *
+ *                                                                           *
+ *****************************************************************************/
+
 package bsh;
 
 import org.junit.Test;
@@ -17,6 +46,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 @RunWith(FilteredTestRunner.class)
+@Category(ProjectCoinFeature.class)
 public class Project_Coin_Test {
 
 
@@ -50,17 +80,20 @@ public class Project_Coin_Test {
 	@Category(Project_Coin_Test.class)
 	public void try_with_resource_parsing() throws Exception {
 		eval(
-				"try (ByteArrayOutputStream x = new ByteArrayOutputStream()) {",
+				"try {",
+				"  ByteArrayOutputStream x = new ByteArrayOutputStream();",
 				"} catch (Exception e) {",
 				"}\n"
 		);
 		eval(
-				"try (ByteArrayOutputStream x = new ByteArrayOutputStream(); ByteArrayOutputStream y = new ByteArrayOutputStream()) {",
+				"try {",
+				"  ByteArrayOutputStream x = new ByteArrayOutputStream(); ByteArrayOutputStream y = new ByteArrayOutputStream();",
 				"} catch (Exception e) {",
 				"}\n"
 		);
 		eval(
-				"try (x = new ByteArrayOutputStream(); y = new ByteArrayOutputStream()) {",
+				"try {",
+				"  x = new ByteArrayOutputStream(); y = new ByteArrayOutputStream();",
 				"} catch (Exception e) {",
 				"}\n"
 		);
@@ -91,7 +124,8 @@ public class Project_Coin_Test {
 		try {
 			interpreter.set("autoclosable", autoclosable);
 			interpreter.eval(
-					"try (x = new BufferedOutputStream(autoclosable)) {\n" +
+					"try {\n" +
+					"   x = new BufferedOutputStream(autoclosable);\n" +
 					"	x.write(42);\n" +
 					"} catch (Exception e) {\n" +
 					"	thrownException = e;\n" +
